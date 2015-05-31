80b6090b18c8ab0f246b58be8d981df572b6573c
thomas grice <augustus.grice@gmail.com>
2015-05-31 15:14:56 -0500

    * remove compile function * clean up

--- directive.js
+++ directive.js
@@ -1,8 +1,8 @@
 var app = angular.module('zdc_ravetext', []);
 
-app.directive('zdcRavetext', ['$interval','$compile', function($interval, $compile){
-    var style = "{'background-image': zdcRtConfig.output.background," +
-        "'background-image': zdcRtConfig.output.background2," +
+app.directive('zdcRavetext', ['$compile', function($compile){
+    var style = "{'background-image': 'linear-gradient(0deg, ' + zdcRtConfig.output.background + ')'," +
+        "'background-image': '-webkit-linear-gradient(0deg, ' + zdcRtConfig.output.background + ')'," +
         "'background-size': '1000% 1000%', " +
         "'color': 'transparent'," +
         "'-webkit-background-clip': 'text'," +
@@ -11,39 +11,32 @@ app.directive('zdcRavetext', ['$interval','$compile', function($interval, $compi
         "'-moz-animation': 'AnimationName 1s ease infinite', " +
         "'-o-animation': 'AnimationName 1s ease infinite', " +
         "'animation': 'AnimationName 1s ease infinite'}";
+
+    var linkFunction = function(scope, elem, attrs){
+        elem.text('<span class="" ng-style="' + style + '">' + elem.html() + '</span>');
+        scope.zdcRtConfig.output = {};
+        scope.zdcRtConfig.output.background = zdcRt.bckgImage(scope.zdcRtConfig.background);
+        htmlText = elem.text();
+        template = angular.element($compile(htmlText)(scope));
+        elem.replaceWith(template);
+    };
+
     var zdcRt = {
-        bckgPrefix: "linear-gradient(235deg, ",
-        bckgPrefix2: " -webkit-linear-gradient(235deg, ",
-        bckgImage: function (input, type){ //takes input of hex color codes and formats them
-            var prefix  = this[type];
-            var suffix = ")";
-            var output = prefix;
+        bckgImage: function (input){ //takes input of hex color codes array and formats them
+            var output = '';
             angular.forEach(input, function(color, index){
                 output = output + color;
                 if(index !== (input.length -1)){
                     output = output + ', ';
-                } else {
-                    output = output + suffix;
                 }
             });
             return output;
         }
     };
-    var linkFunction = function(scope, elem, attrs){
-        scope.zdcRtConfig.output = {};
-        scope.zdcRtConfig.output.background = zdcRt.bckgImage(scope.zdcRtConfig.background, 'bckgPrefix');
-        scope.zdcRtConfig.output.background2 = zdcRt.bckgImage(scope.zdcRtConfig.background, 'bckgPrefix2');
-        htmlText = elem.text();
-    template = angular.element($compile(htmlText)(scope));
-    elem.replaceWith(template);
-  };
 
   return {
     restrict: 'E',
     scope: true,
-    compile: function compile(elem, attr){
-      elem.text('<span class="" ng-style="' + style + '">' + elem.html() + '</span>');
-      return linkFunction;
-    }
+      link: linkFunction
   }
 }]);
--- example/app.js
+++ example/app.js
@@ -2,7 +2,7 @@ var app = angular.module('zdc_ravetext_example', ['zdc_ravetext']);
 
 app.controller('MainCtrl', function($scope) {
     $scope.zdcRtConfig = {
-        background  :['#246655', '#cec763', '#c463ce', '#63bace', '#3046bb']
+        background  :['#246655', '#cec763', '#c463ce', '#63bace', '#3046bb', '#000000']
         };
   $scope.name = 'World';
 });
